1886d738dd77ff409dc265cc5baedffa9e9f301c
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-09-28 20:14:24 -0500

    Pelota que rebota en el piso

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+let posX, posY, diam, rad;
+let esp = 34;
+let margen = 50;
+let piso;
+let velY = 10;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  posX = windowWidth / 2;
+  posY = 100;
+  rectMode(CENTER);
+  frameRate(25);
+  diam = 50;
+  rad = diam / 2;
+  piso = windowHeight - margen - esp / 2;
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(0, 220, 210, 50);
+  noStroke();
+
+  posY += velY;
+
+  if (posY > piso - rad) {
+    print("se choco");
+    velY *= -1;
+  }
+  if (posY < 0 + rad) {
+    print("arriba");
+    velY *= -1;
+  }
+  fill(255);
+  circle(posX, posY, diam);
+
+  fill(255, 0, 0);
+  rect(windowWidth / 2, windowHeight - 50, windowWidth * 0.8, esp);
 }

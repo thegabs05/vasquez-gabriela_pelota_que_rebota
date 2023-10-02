143123a6dd6a8636e74220be7b086c9ae8005d89
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-10-02 15:18:56 -0500

    Pelota que rebota con aceleracion

--- sketch.js
+++ sketch.js
@@ -1,37 +1,53 @@
 let posX, posY, diam, rad;
-let esp = 34;
-let margen = 50;
+let esp = 50;
+let margen = 40;
 let piso;
-let velY = 10;
+let velY = 0;
+let acel = 0.98;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   posX = windowWidth / 2;
-  posY = 100;
+  posY = windowHeight * 0.2;
   rectMode(CENTER);
-  frameRate(25);
-  diam = 50;
+  //frameRate(35);
+  diam = random(10, 50);
   rad = diam / 2;
   piso = windowHeight - margen - esp / 2;
 }
 
 function draw() {
   background(0, 220, 210, 50);
+
+  update();
+
   noStroke();
 
-  posY += velY;
+  //posY += velY;
 
-  if (posY > piso - rad) {
-    print("se choco");
-    velY *= -1;
-  }
-  if (posY < 0 + rad) {
-    print("arriba");
-    velY *= -1;
-  }
+  //if (posY > piso - rad) {
+  //  print("se choco");
+  //  velY *= -1;
+  // }
+  //if (posY < 0 + rad) {
+  //  print("arriba");
+  //  velY *= -1;
+  // }
   fill(255);
   circle(posX, posY, diam);
 
   fill(255, 0, 0);
-  rect(windowWidth / 2, windowHeight - 50, windowWidth * 0.8, esp);
+  rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
+}
+
+function update() {
+  velY += acel;
+  posY += velY;
+
+  if (posY >= piso - rad) {
+    //posY = piso - rad;
+    //print("se choco");
+    velY *= -1;
+    posY += velY;
+  }
 }

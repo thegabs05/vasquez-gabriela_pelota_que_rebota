1b4f163e6e1d84c7be837841d0d8a330937f1d86
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-10-20 20:06:20 -0500

    pelota que rebota final

--- sketch.js
+++ sketch.js
@@ -1,59 +1,18 @@
-// let posX, posY, diam, rad;
-// let esp = 50;
-// let margen = 40;
-// let piso;
-// let velY = 0;
-// let acel = 0.98;
 let pelota1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pelota1 = new pelotarebota(windowWidth / 2, windowHeight * 0.2);
-
-  // posX = windowWidth / 2;
-  // posY = windowHeight * 0.2;
-  // rectMode(CENTER);
-  // // //frameRate(35);
-  // diam = random(10, 50);
-  // rad = diam / 2;
-  // piso = windowHeight - margen - esp / 2;
 }
 
 function draw() {
-  // background(0, 220, 210, 50);
-  // update();
-  // noStroke();
-  //posY += velY;
-  //if (posY > piso - rad) {
-  //  print("se choco");
-  //  velY *= -1;
-  // }
-  //if (posY < 0 + rad) {
-  //  print("arriba");
-  //  velY *= -1;
-  // }
-  // fill(255);
-  // circle(posX, posY, diam);
-  // fill(255, 0, 0);
-  // rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
   pelota1.update();
   pelota1.display();
 }
 
-function update() {
-  // velY += acel;
-  // posY += velY;
-  // if (posY >= piso - rad) {
-  //   //posY = piso - rad;
-  //   //print("se choco");
-  //   velY *= -1;
-  //   posY += velY;
-  // }
-}
-
 class pelotarebota {
   constructor(_posX, _posY) {
-    this.posX = _posX;
+    this.posX = random(windowWidth * 0.1, windowWidth * 0.9);
     this.posY = _posY;
     this.diam = random(10, 50);
     this.rad = this.diam / 2;
